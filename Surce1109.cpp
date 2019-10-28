52fb2eb83ce22b8eb77461ac41e467018133a044
Volodymir Konovalov <vvkon@meta.ua>
2019-10-28 10:35:35 +0200

    Update Surce1109.cpp

--- Surce1109.cpp
+++ Surce1109.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    cout << "To convert a binary file to text, press 1; \n"
+    cout << "\n\nTo convert a binary file to text, press 1; \n"
          << "to reverse convert a text file to binary, press 2;\n"
          << "to exit, press 3:\n";
         char flag;
@@ -12,6 +12,7 @@ int main()
                 {
                     cout << "Enter the name of the text file to enter:\n";
                     string iname;
+                    cin.get();
                     getline(cin, iname);
                     ifstream ifs(iname);
                     if(!ifs) error("Unable to open input file ", iname);
