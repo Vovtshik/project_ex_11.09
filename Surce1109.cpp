a6aea0b36afdce6ede4e0ee4613445da8afda01c
Volodymir Konovalov <vvkon@meta.ua>
2019-11-05 06:32:54 +0200

    Update Surce1109.cpp

--- Surce1109.cpp
+++ Surce1109.cpp
@@ -5,74 +5,10 @@ void binery_file_to_text_file(vector<string>& vs);
 
 int main()
 {
-	/* cout << "\n\nTo convert a text file to binary, press 1; \n"
-		<< "to reverse convert a binary file to text, press 2;\n"
-		<< "to exit, press 3:\n"; */
 	char flag;
 	vector<string> vs;
 	text_file_to_binery_file(vs);
 	binery_file_to_text_file(vs);
-	/* vector<string> vs2; */
-	/* while (cin >> flag)
-	{
-		if (flag == '1')
-		{
-			cout << "Enter the name of the text file to enter:\n";
-			string iname;
-			cin.get();
-			getline(cin, iname);
-			ifstream ifs(iname);
-			if (!ifs) error("Unable to open input file ", iname);
-			for (string s; getline(ifs, s);)
-			{
-				vs.push_back(s);
-			}
-			cout << "Enter the file name for the binary entry:\n";
-			string oname;
-			getline(cin, oname);
-			ofstream ofs{ oname, ios_base::binary };
-			if (!ofs) error("Unable to open output file", oname);
-			ofs.write(as_bytes(vs), sizeof vs);
-			cout << "\n\nTo convert a text file to binary, press 1; \n"
-				<< "to reverse convert a binary file to text, press 2;\n"
-				<< "to exit, press 3:\n";
-		}
-		else if (flag == '2')
-		{
-			cout << "Enter the file name for binary reading:\n";
-			string iname;
-			cin.get();
-			getline(cin, iname);
-			ifstream ifs(iname, ios_base::binary);
-			if (!ifs) error("Unable to open input file ", iname);
-			ifs.read(as_bytes(vs2), sizeof vs);
-			cout << "Enter the file name for the text entry:\n";
-			string oname;
-			getline(cin, oname);
-			ofstream ofs(oname);
-			for (string s: vs2)
-			{
-				ofs << s << '\n';
-			}
-			cout << "\n\nTo convert a text file to binary, press 1; \n"
-				<< "to reverse convert a binary file to text, press 2;\n"
-				<< "to exit, press 3:\n";
-		}
-		else if (flag == '3')
-			break;
-		else
-		{
-			cout << "Mistake! Repeat entry:\n"
-				<< "to convert a binary file to text, press 1; \n"
-				<< "to reverse convert a text file to binary, press 2;\n"
-				<< "to exit, press 3:\n";
-			char ch;
-			while (cin.get(ch) && ch != '\n')
-			{
-				continue;
-			}
-		}
-	} */
 	return 0;
 }
 
@@ -80,7 +16,6 @@ void text_file_to_binery_file(vector<string>& vs)
 {
 	cout << "Enter the name of the text file to enter:\n";
 			string iname;
-			// cin.get();
 			getline(cin, iname);
 			ifstream ifs(iname);
 			if (!ifs) error("Unable to open input file ", iname);
@@ -100,7 +35,6 @@ void binery_file_to_text_file(vector<string>& vs)
 {
 	cout << "Enter the file name for binary reading:\n";
 			string iname;
-			// cin.get();
 			getline(cin, iname);
 			ifstream ifs(iname, ios_base::binary);
 			if (!ifs) error("Unable to open input file ", iname);
